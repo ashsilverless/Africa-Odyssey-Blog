7dec56ce319d6ed2530ddbe29632d7c667a897e8
Ash - SIlverless <ash@silverless.co.uk>
2020-03-10 15:01:22 +0000

    Fix - Meta removed

--- header.php
+++ header.php
@@ -16,8 +16,7 @@
 <head>
 
 <meta charset="UTF-8">
-<meta name="description" content=" ">
-<meta name="keywords" content=" ">
+<!--Removed 10-3-20-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
 <title></title>
